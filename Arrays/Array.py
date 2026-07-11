51bab8c6b9970f9a93fbcad58c3665084699fcdf
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-07-11 18:26:29 +0530

    learning about methods and properties of arrary in python

--- Arrays/Array.py
+++ Arrays/Array.py
@@ -4,7 +4,7 @@ import array
 from array import *
 # or we can use this to import all the functions and classes from the array module.
 
-val = array.array('i', [1, 2, 3, 4, 5])
+val = array('i', [1, 2, 3, 4, 5])
 # print(val)
 
 for i in range(0,5):
@@ -14,3 +14,25 @@ print("\n")
 
 for x in val:
   print(x, end=", ")
+
+# print("\n")
+
+# print(val.typecode)
+# val.reverse()
+
+# for i in val:
+#   print(i, end=" ")
+
+val.insert(1, 50)
+val.append(60)
+val[2] = 100
+
+copyArray = array(val.typecode, (x*2 for x in val))
+
+copyArray.pop()
+# copyArray.remove(100) to remove a specific value from the array.
+
+print("\n")
+
+for i in copyArray:
+  print(i, end=" ")
