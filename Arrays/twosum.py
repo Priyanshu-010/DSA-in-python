bce80a1982cc7404bfdda5978f8c8f31855d5ee0
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-07-14 21:01:28 +0530

    two sum problem in python

---
+++ Arrays/twosum.py
@@ -0,0 +1,12 @@
+nums = [2,11,7,15]
+target = 9
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if(nums[i] + nums[j] == target):
+                return [i,j]
+    return []
+
+
+print(twoSum(nums, target))
