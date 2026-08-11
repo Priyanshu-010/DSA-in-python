1daadd87c006dcfc3a499809de2538ab8fb47fdb
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-08-11 11:40:11 +0530

    hashing

---
+++ hashing/usingDict.py
@@ -0,0 +1,22 @@
+n = [5,6,3,1,1,4,5,10,9,9,7]
+m = [10,111,321,4,25,5,7,21]
+
+dict={}
+max = 0
+elem = 0
+for i in n:
+  dict[i] = dict.get(i, 0)+1
+
+for k in dict:
+  if dict[k]>max:
+    max = dict[k]
+    elem = k
+print(max)
+print(elem)
+
+for j in m:
+  if j<0 or j>10:
+    print(0, end=" ")
+  else:
+    print(dict.get(j,0), end=" ")
+
