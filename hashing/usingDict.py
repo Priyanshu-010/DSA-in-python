d7f754f87952e8563cdee080082955c22addb49b
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-08-11 11:41:44 +0530

    comments

--- hashing/usingDict.py
+++ hashing/usingDict.py
@@ -18,5 +18,9 @@ for j in m:
   if j<0 or j>10:
     print(0, end=" ")
   else:
-    print(dict.get(j,0), end=" ")
+    print(dict.get(j,0), end=" ") # Why we used 0 because if the key is not present in the dictionary, it will return 0 instead of throwing an error. This way, we can safely get the count of occurrences for each element in list m without worrying about missing keys.
+
+# What we did in this code is we created a dictionary to count the occurrences of each element in list n. Then, we found the element with the maximum occurrences and printed it. Finally, we iterated through list m and printed the count of each element based on the dictionary we created, handling cases where the element is not present in the dictionary by returning 0.
+
+
 
