e4c67e0be6baba0f6f01fc23f318bd2fac942c3a
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-08-20 13:21:49 +0530

    optimal solution of right rotate an array by k place

--- array/easy/rightRotateByKPlace.py
+++ array/easy/rightRotateByKPlace.py
@@ -10,6 +10,26 @@ nums[:] = nums[n-k:] + nums[:n-k]
 
 print(nums)
 
+# one more optimal solution
+
+# n = len(nums)
+# k = k%n
+# if k == 0:
+#   print(nums)
+
+# def reverse(nums, left, right):
+#   while left<right:
+#     nums[left], nums[right]= nums[right], nums[left]
+#     left +=1
+#     right -=1
+
+# reverse(nums,n-k,n-1) # reverse Last K elements
+# reverse(nums,0,n-k-1) # reverse remaining elements
+# reverse(nums,0, n-1 ) # reverse the whole array
+
+# print(nums)
+
+
 # Brute Force
 
 # n = len(nums)
