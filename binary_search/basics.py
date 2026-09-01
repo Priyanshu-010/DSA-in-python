f05a554a24db19d493a8f2470d37f34dd8d8d4f9
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-09-01 20:29:38 +0530

    recursive binary search

--- binary_search/basics.py
+++ binary_search/basics.py
@@ -15,3 +15,23 @@ while low <= high:
     high = mid-1
   else:
     print(-1)
+
+# Time Complexity: O(log n), Space complexity: O(1)
+
+
+def bs(nums, low, high,x):
+  if low > high:
+    return -1
+  
+  mid = (low+high)//2
+  if nums[mid] == x:
+    return mid
+  elif nums[mid] < x:
+    return bs(nums,mid+1, high,x)
+  else:
+    return bs(nums,low, mid-1,x)
+
+
+print(bs(nums,low,high,target))
+
+# Time Complexity: O(log n), Space complexity: O(1)
