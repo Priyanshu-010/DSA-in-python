f087e324ff04abd466a1d02afc5dff2f8b07ecf7
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-09-08 22:07:37 +0530

    count occurence

---
+++ binary_search/count_occurence.py
@@ -0,0 +1,25 @@
+nums = [1,2,3,3,3,3,3,4,5,6,8,9,9,10]
+
+target = 5
+
+# Brute force approach
+
+# f = -1
+# l= -1
+
+# for i in range(len(nums)):
+#   if nums[i] == target:
+#     if f == -1:
+#       f = i
+#     l = i
+# if f == -1:
+#   print(0)
+
+# print((l-f)+1)
+
+# Time complexity: O(n), Space complexity: O(1)
+
+# Optimized approach
+
+
+
