e842c497d2b73e9e1f7c6f837eca48a61d1fbe88
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-09-15 18:14:34 +0530

    Basics and middle of the linked list

--- linkedlist/basics.py
+++ linkedlist/basics.py
@@ -3,15 +3,107 @@ class Node():
     self.val = val
     self.next = None
 
-node1 = Node(1)
-node2 = Node(2)
+node1 = Node(5)
+node2 = Node(7)
 node3 = Node(3)
-node4 = Node(4)
+node4 = Node(1)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
 
-print(node1.val)
-print(node2.val)
-print(node1.next.next.val)
+head = node1
+
+# print(head.val)
+# print(node2.val)
+# print(node1.next.next.val)
+
+# traverse a linked list
+def print_linked_list(head):
+  curr = head
+
+  while curr!= None:
+    print(curr.val, end=' ')
+    curr = curr.next
+
+# ================================================================================
+
+# insertion at the beginning
+
+# newNode= Node(4)
+
+# newNode.next= head
+# head = newNode
+
+# print_linked_list(head)
+
+# ================================================================================
+
+# insertion at end
+
+# lastNode = Node(8)
+
+# curr = head
+
+# while curr.next != None:
+#   curr = curr.next
+
+# curr.next = lastNode
+
+
+# print_linked_list(head)
+
+
+#===============================================================================
+
+# Insertion at Kth index
+
+# k = 2
+
+# curr = head
+# for i in range(k-1):
+#   curr = curr.next
+
+# newNode = Node(4)
+# newNode.next = curr.next
+# curr.next = newNode
+
+# print_linked_list(head)
+
+# explanation of the code:
+# new node ke next ko curr ke next pe point kardo or curr ke next ka new node ko point kardo is se newNode desired index pe insert hojayega
+
+# ================================================================================
+
+# delete the first Node
+
+# head = head.next
+# print_linked_list(head)
+
+# ================================================================================
+
+# delete the last Node
+
+# curr = head
+
+# while curr.next.next !=None:
+#   curr = curr.next
+
+# curr.next = None
+
+# print_linked_list(head)
+
+# ================================================================================
+
+# delete the Kth Node
+
+k = 2
+
+curr = head
+for i in range(k-1):
+  curr = curr.next
+newNode = Node(4)
+
+curr.next = curr.next.next
+
+print_linked_list(head)
---
+++ linkedlist/middleOfTheList.py
@@ -0,0 +1,44 @@
+# leetcode 876. Middle of the Linked List
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+class Node():
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+node1 = Node(5)
+node2 = Node(7)
+node3 = Node(3)
+node4 = Node(1)
+node5 = Node(2)
+node6 = Node(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+
+head = node1
+
+curr = head
+l = 0
+while curr!= None:
+    curr = curr.next
+    l += 1
+
+curr = head
+newL = l//2
+
+for i in range(newL):
+    curr = curr.next
+
+
+def print_linked_list(head):
+  curr = head
+
+  while curr!= None:
+    print(curr.val, end=' ')
+    curr = curr.next
+
+print_linked_list(curr)
